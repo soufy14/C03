28cee30c829e5b6b4d5791bcd96c027c9a97bf8c
soufy14 <soufiyacouba@gmail.com>
2025-05-18 17:38:16 +0100

    bash

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,10 +4,7 @@ int main() {
     int n, i;
     long long t1 = 0, t2 = 1, temp;
     scanf("%d", &n);
-    if (n < 0) {
-        printf("Erreur.\n");
-        return 1;
-    }
+    
     for (i = 1; i <= n; i++) {
         printf("%lld ", t1);
         temp = t1 + t2;
