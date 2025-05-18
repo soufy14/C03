d8766f861123685bbc080b7e5c4890175df1e8c6
soufy14 <soufiyacouba@gmail.com>
2025-05-18 17:30:24 +0100

    bash

---
+++ fibonacci_series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i;
+    long long t1 = 0, t2 = 1, temp;
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Erreur.\n");
+        return 1;
+    }
+    for (i = 1; i <= n; (i++)) {
+        printf("%lld ", t1);
+        temp = t1 + t2;
+        t1 = t2;
+        t2 = temp; 
+    }
+    printf("\n");
+    return 0;
+}
