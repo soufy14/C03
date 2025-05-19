8cc0f8fc5d5773ef7c3e55972e2a8124eaaf8732
soufy14 <soufiyacouba@gmail.com>
2025-05-19 14:56:21 +0100

    c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -5,21 +5,17 @@ int main() {
     int a = 0, b = 1, temp;
 
     scanf("%d", &n);
-    if (n > 20 || n < 1) {
-        printf("n doit etre:1 a 20\n");
+    if (n > 20 || n < 0) {
+        printf("Erreur\n");
         return 1;
     }
-
-    while (a <= n) {
+     while (a <= n) {
         printf("%d ", a);
         temp = a + b;
         a = b;
         b = temp;
-
     }
-
-    printf("\n");
-    
+    printf("\n"); 
     return 0;
 }
 
