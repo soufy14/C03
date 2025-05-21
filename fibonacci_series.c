16fc89931463a0cf6dc4b92dcc99e5828ecd52a7
soufy14 <soufiyacouba@gmail.com>
2025-05-21 18:52:55 +0100

    c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -3,12 +3,12 @@ int main() {
     int n;
     int a = 0, b = 1, temp;
     scanf("%d", &n);
-    if (n <= 0) {
+    if (n > 20) {
         printf("Erreur\n");
         return 1;
     }
     while (a <= n) {
-        printf("%d", a);
+        printf("%d.", a);
         temp = a + b;
         a = b;
         b = temp;
