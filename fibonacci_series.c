a76a8304703f2d08e23882f049c4e1abe749f86f
soufy14 <soufiyacouba@gmail.com>
2025-05-21 12:26:46 +0100

    c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main() {
+
     int n;
     int a = 0, b = 1, temp;
 
+
     scanf("%d", &n);
-    if (n < 20) {
+
+    if (n <= 0) {
         printf("Erreur\n");
         return 1;
     }
-     while (a <= n) {
+
+    while (a <= n) {
         printf("%d ", a);
         temp = a + b;
         a = b;
         b = temp;
     }
-    printf("\n"); 
+
+    printf("\n");
     return 0;
 }
-
 // c'est programme affiche  la série de Fibonacci jusqu'à une valeur limite donnée.
