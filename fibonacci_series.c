cab57f46cb723949e3938a75970846afdf9dcb2c
soufy14 <soufiyacouba@gmail.com>
2025-05-21 14:59:03 +0100

    c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-
 int main() {
-
     int n;
     int a = 0, b = 1, temp;
-
     scanf("%d", &n);
-
     if (n <= 0) {
         printf("Erreur\n");
         return 1;
     }
-
     while (a <= n) {
-        printf("%d ", a); // Ajout d'un espace pour lisibilité
+        printf("%d", a);
         temp = a + b;
         a = b;
         b = temp;
     }
-
     printf("\n");
     return 0;
 }
